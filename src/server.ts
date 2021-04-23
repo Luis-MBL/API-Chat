02a6327cd63ae6a806cf6caf7a96bfeec7b5dad9
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-22 22:04:08 -0300

    feature: add status route

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.json({
+    version: '1.0',
+    owner: 'Luis-MBL',
+  });
+});
+
 app.listen(5003, () => console.log('▶️ Server is running on port 5003'));
