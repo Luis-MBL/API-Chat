23da24e438655fe0a11d9400933a3761465410bc
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-25 17:56:16 -0300

    feature: add create settings route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
 import './database';
+import { routes } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) => {
   response.json({
