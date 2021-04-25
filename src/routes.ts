51cb4c63c841ba31f2d1305aee12c1b462a176b5
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-25 17:59:58 -0300

    chore: create SettingsController

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,10 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from './repositories/SettingsRepository';
+import { SettingsController } from './controllers/SettingsController';
+
+const settingsController = new SettingsController();
 
 const routes = Router();
 
-routes.post('/settings', async (request, response) => {
-  const { chat, username } = request.body;
-
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const setting = settingsRepository.create({
-    chat,
-    username,
-  });
-
-  await settingsRepository.save(setting);
-
-  response.json(setting);
-});
+routes.post('/settings', settingsController.create);
 
 export { routes };
