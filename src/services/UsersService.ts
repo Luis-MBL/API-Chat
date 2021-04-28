2abd2f6c1c93a34725162fac2fe3d85613e5f871
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-28 20:40:38 -0300

    feat: create user service

---
+++ src/services/UsersService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+import { User } from '../entities/User';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+}
+class UsersService {
+  async create({ email }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userExists = await usersRepository.findOne({ email });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = usersRepository.create({ email });
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+export { UsersService };
