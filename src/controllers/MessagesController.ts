50261c487ae875e7d33ae4ba88b51a6c7c1c9940
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-28 21:39:31 -0300

    feat: ShowByUser in message controller

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -14,5 +14,13 @@ class MessagesController {
 
     return response.json(message);
   }
+
+  async showByUser(request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.params;
+    const messagesService = new MessagesService();
+
+    const messages = await messagesService.listByUser(user_id);
+    return response.json(messages);
+  }
 }
 export { MessagesController };
