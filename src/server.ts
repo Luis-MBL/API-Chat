017033dfe3ded916824a97fd86ac58cd0bae0c36
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-29 20:31:28 -0300

    feat: create socket connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
 
 import './database';
 import { routes } from './routes';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/pages/client', (request, response) => {
+  return response.render('html/client.html');
+});
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+  console.log('Link Start', socket.id);
+});
 
 app.use(express.json());
 app.use(routes);
@@ -15,4 +34,4 @@ app.get('/', (request, response) => {
   });
 });
 
-app.listen(5003, () => console.log('▶️ Server is running on port 5003'));
+http.listen(5003, () => console.log('▶️ Server is running on port 5003'));
