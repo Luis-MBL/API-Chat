1047f44ccb68f35e02da5bc18d635d168014180e
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-04-29 21:29:06 -0300

    feat: add hooks in client.ts

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,52 @@
 import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
+import { MessagesService } from '../services/MessagesService';
+import { UsersService } from '../services/UsersService';
 
 io.on('connect', (socket) => {
-  socket.on('client_first_access', (params) => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
+
+  interface IParams {
+    text: string;
+    email: string;
+  }
+
+  socket.on('client_first_access', async (params) => {
+    const socket_id = socket.id;
+    const { email, text } = params as IParams;
+    let user_id;
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      const user = await usersService.create({ email });
+      user_id = user.id;
+      await connectionsService.create({
+        socket_id,
+        user_id,
+      });
+    } else {
+      user_id = userExists.id;
+      await connectionsService.create({
+        socket_id,
+        user_id,
+      });
+      const connection = await connectionsService.findByUserId(userExists.id);
+      if (!connection) {
+        await connectionsService.create({
+          socket_id,
+          user_id: userExists.id,
+        });
+      } else {
+        connection.socket_id = socket_id;
+        await connectionsService.create(connection);
+      }
+    }
+
+    await messagesService.create({
+      text,
+      user_id,
+    });
   });
 });
